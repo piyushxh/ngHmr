6c747b4ce584b69e402966f1c6255196d4de6199
Shawn Flahave <shawnflahave@gmail.com>
2016-12-24 13:27:44 -0600

    updated README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works pretty, prettay, prettttaaaayyy super!';
+  title = 'app works pretty, prettay, good!';
 }
